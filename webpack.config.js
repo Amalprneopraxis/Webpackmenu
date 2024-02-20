9eb10cbe8c3f10bb28f4d3a3abefcdf61e6f0335
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-20 16:43:28 +0530

    simplify the react hook integration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// webpack.config.js
+const path = require('path');
+const webpack = require('webpack'); // Import webpack module
+
+module.exports = {
+  mode: 'production',
+  entry: './src/Navigation.js', // Path to your component entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'Navigation.bundle.[chunkhash].js', // Output filename
+    library: 'Navigation', // Name of the library
+    libraryTarget: 'umd', // Output module format
+    umdNamedDefine: true, // Define the module's name for AMD
+    globalObject: 'this', // Define the global object used,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              "@babel/plugin-proposal-class-properties", // Allows class properties syntax
+              "@babel/plugin-transform-runtime", // Helps with async/await and other features
+              "@babel/plugin-syntax-dynamic-import" // Enables support for dynamic import() syntax
+            ]
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    // Add any other plugins you need here
+  ]
+};
