cdc12fe56b4592d2a63342eaad9bdc046a7e7416
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-23 12:51:00 +0530

    firt commit for configuration

---
+++ src/Navigationmenu.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './components/header/Megamenu.css'
+
+const Navigationmenu = () => {
+    const [slideIndex, setSlideIndex] = useState('1');
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        plusSlides(1);
+      }, 4000);
+      return () => clearInterval(interval);
+    }, [slideIndex]);
+  
+    const plusSlides = (n) => {
+      showSlides(slideIndex + n);
+    };
+  
+    const showSlides = (n) => {
+      let newIndex = n;
+      const slides = document.getElementsByClassName("mySlides");
+      if (n > slides.length) {
+        newIndex = 1;
+      }
+      if (n < 1) {
+        newIndex = slides.length;
+      }
+      setSlideIndex(newIndex);
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slides[newIndex - 1].style.display = "block";
+    };
+  
+    return (
+      <div className="slideshow-container">
+        <div className="mySlides">
+          <q style={{color: "red"}}>............I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
+        </div>
+        <div className="mySlides">
+          <q style={{color: "red"}}>But man is not made for defeat.</q>
+        </div>
+        <div className="mySlides">
+          <q style={{color: "red"}}> I have not failed. I've just found 10,000 ways that won't work.</q>
+        </div>
+      </div>
+    );
+  };
+  // Attach Headernav to the window object
+   window.Navigationmenu = Navigationmenu;
+   export default Navigationmenu;
---
+++ src/components/common/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const  Navigation = () =>{
+
+    return(
+        <>
+        <div>
+            <h6>This is the component from Navigation menu</h6>
+        </div>
+        </>
+    );
+};
+
+export default Navigation;
